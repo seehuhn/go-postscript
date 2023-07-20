6f0ea5f722bea5c9ae15cdc731491e2371c08ad4
Jochen Voss <voss@seehuhn.de>
2023-07-20 17:07:20 +0100

    updates

---
+++ pfb/doc.go
@@ -0,0 +1,21 @@
+// seehuhn.de/go/postscript - a rudimentary PostScript interpreter
+// Copyright (C) 2023  Jochen Voss <voss@seehuhn.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// Package pfb implements the PFB (Printer Font Binary) format.
+//
+// This PFB format is documented in section 3.3 of Adobe's technical note #5040
+// (Supporting Downloadable PostScript Language Fonts).
+package pfb
